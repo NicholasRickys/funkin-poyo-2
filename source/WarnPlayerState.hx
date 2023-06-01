b961e08c9155dca9c435fb0841cf15aa9790d38c
Nicholas Demetro <nicholas.rickys3@gmail.com>
2023-06-01 16:57:28 -0400

    work

--- source/WarnPlayerState.hx
+++ source/WarnPlayerState.hx
@@ -1,12 +1,12 @@
 package;
 
 import flixel.FlxG;
-import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.util.FlxTimer;
 import flixel.tweens.FlxTween;
+import flixel.addons.ui.FlxUIState;
 
-class WarnPlayerState extends FlxState {
+class WarnPlayerState extends FlxUIState {
 	var warningText:FlxText;
 	var warningStr:String = "If you are prone to elipesy, VS Poyo Ultimate has a high chance to trigger seizures.\nWe don't have anyway to disable this as of right now.\nYou have been warned!";
 
