baba6ad0fbf9c37aa36caa9b9faac8b31b7b20d8
Nicholas Demetro <nicholas.rickys3@gmail.com>
2023-06-01 15:49:08 -0400

    hilarious

--- source/WarnPlayerState.hx
+++ source/WarnPlayerState.hx
@@ -9,23 +9,25 @@ class WarnPlayerState {
 	var warningText:FlxText;
 	var warningStr:String = "If you are prone to elipesy, VS Poyo Ultimate has a high chance to trigger seizures.\nWe don't have anyway to disable this as of right now.\nYou have been warned!";
 
-	warningText = new FlxText(0,0,0,warningStr,FlxG.width);
-	warningText.screenCenter();
-
-	warningText.alpha = 0;
-	warningText.alignment = FlxTextAlign.CENTER;
-
-	add(warningText);
-
-	FlxTween.tween(warningText, {alpha: 1}, 0.5, {
-		onComplete: function() {
-			new FlxTimer().start(5, ()->{
-				FlxTween.tween(warningText, {alpha: 1}, 0.5, function() {
-					onComplete: function() {
-						FlxG.switchState(TitleState);
-					}
+	function create() {
+		warningText = new FlxText(0,0,0,warningStr,FlxG.width);
+		warningText.screenCenter();
+	
+		warningText.alpha = 0;
+		warningText.alignment = FlxTextAlign.CENTER;
+	
+		add(warningText);
+	
+		FlxTween.tween(warningText, {alpha: 1}, 0.5, {
+			onComplete: function() {
+				new FlxTimer().start(5, ()->{
+					FlxTween.tween(warningText, {alpha: 1}, 0.5, function() {
+						onComplete: function() {
+							FlxG.switchState(TitleState);
+						}
+					});
 				});
-			});
-		}
-	});
+			}
+		});
+	}
 }
