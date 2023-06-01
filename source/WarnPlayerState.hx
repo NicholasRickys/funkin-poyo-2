58f6a004a8dd4f8fc006b4f22ee2aa1abf695e19
Nicholas Demetro <nicholas.rickys3@gmail.com>
2023-06-01 18:52:18 -0400

    fuck fancy shit, im too dumb

--- source/WarnPlayerState.hx
+++ source/WarnPlayerState.hx
@@ -8,28 +8,19 @@ import flixel.tweens.FlxTween;
 
 class WarnPlayerState extends FlxState {
 	var warningText:FlxText;
-	var warningStr:String = "If you are prone to elipesy, VS Poyo Ultimate has a high chance to trigger seizures.\nWe don't have anyway to disable this as of right now.\nYou have been warned!";
+	var warningStr:String = "In honor of Beans\n\nIf you are prone to elipesy, VS Poyo Ultimate has a high chance to trigger seizures.\nWe don't have anyway to disable this as of right now.\nYou have been warned!";
 	var letsWait:FlxTimer;
 
 	override function create() {
 		warningText = new FlxText(0,0,0,warningStr,FlxG.width);
 	
-		warningText.alpha = 0;
 		warningText.alignment = FlxTextAlign.CENTER;
 		warningText.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(warningText);
 		warningText.screenCenter();
-	
-		FlxTween.tween(warningText, {alpha: 1}, 0.5, {
-			onComplete: function(tween:FlxTween) {
-				letsWait = new FlxTimer().start(4, function() {
-					FlxTween.tween(warningText, {alpha: 0}, 0.5, {
-						onComplete: function(tween:FlxTween) {
-							FlxG.switchState(new TitleState());
-						}
-					});
-				}, 4);
-			}
+
+		letsWait = new FlxTimer().start(5, function(tmr:FlxTimer):Void {
+			FlxG.switchState(new TitleState());
 		});
 	}
 }
